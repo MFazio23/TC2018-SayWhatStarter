d28d85eb56ca5501bb9830f58f7a9dd8402925a8
Michael Fazio <mfazio@skylinetechnologies.com>
2018-07-31 09:59:12 -0500

    Add node example; Dialogflow export

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # TC2018-SayWhatStarter
-A starter Action on Google for people attending "Say What? Crafting Dialogue for Your Bot"
+A starter Action on Google for people attending "Say What? Crafting Dialogue for Your Bot" at [That Conference 2018](https://www.thatconference.com/sessions/session/11947).
+This could also be helpful for anyone starging with [Dialogflow](https://dialogflow.com/).
+
+## Examples
+The following intents are located in the SayWhatStarter.zip, which can be imported into Dialogflow.
+
+### Basic Response
+Contains a few simple training phrases with a couple of corresponding text responses.
+
+### Basic Fulfillment
+This is similar to the basic response, but the text response is coming from your webhook endpoint rather than Dialogflow.  You can set a webhook URL within the **Fulfillment** section of your agent.
+
+### Entities/Parameters
+This example brings in two parameters (`city` and `room`) to be pulled out of statements and used later.
+Text responses attached to the intent include:
+* A generic response
+* A response when a user states a city
+* A response when a user states a room
+* A response for a room with an explanation about the difference between `$room` and `$room.original`
+* A response with both a city and a room
+
+### Contexts
+Contexts allow your agent to retain certain pieces of information from intent to intent and configure certain intents to only be triggered when in a given context.
+For example, a user can give their current location in one intent and the Dialogflow agent can remember that for a set number of intents after and use that value as needed.  Also, intents listed with the proper input context will only be trigged when that context is activated.
+The example here allows a user to cite their name then respond "yes" or "no" to hearing about how contexts work.  In both examples, the agent is able to reply with their name.
+One other piece of note here is the prompt associated with the `firstName` parameter.  Since we made this value required, we include an additional prompt to get that before moving onto the normal response.  If the user states their name initially, we can just skip the prompt piece.
+
+### Google Assistant
+When using an Action via the Google Assistant on a phone or device with a display, you can send back additional response types.  This includes (but is not limited to) cards, lists, and media content.
+The example can show a basic card, a link out suggestion, and one simple response with differing text and speech responses.
+
+### Default Exit Intent
+This is an intent added to make leaving the Action a bit easier.  Instead of saying "stop" or "cancel", it adds a number of other phrases allowing the user to leave the current Action.  Note the "Set this intent as end of conversation" flag, which will end the conversation when the intent is triggered.  We can also end a conversation within the webhook.
+
+## Pre-Populated Intents
+
+### Default Welcome Intent
+Created and populated automatically by DialogFlow, this intent has the `Welcome` event associated by default.  This is the standard intent used when starting up an Action, usually by saying *"OK Google, talk to \<invocation phrase>"*.  We can respond just like any other intent, either with text responses in Dialogflow, Google Assistant-specific responses, or fulfillment from the webhook.
+
+### Default Fallback Intent
+Created and populated automatically by Dialogflow, this is the intent that's trigged when no other intents are matched.  We can respond just like any other intent, either with text responses in Dialogflow, Google Assistant-specific responses, or fulfillment from the webhook.
+Note that you can actually add specific phrases in this intent that are supposed to not match any other intent.  For more info, see here: [Negative Examples](https://dialogflow.com/docs/intents#negative_examples)
---
+++ node/server.js
@@ -0,0 +1,99 @@
+const {dialogflow, BasicCard, Button} = require('actions-on-google');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const endingQuestion = "What else can I do for you?";
+
+//Utility methods
+const getRandomItemFromArray = (array) => array[Math.floor(Math.random() * array.length)];
+
+const respond = (conv, response, end, noQuestion) => {
+    if (!response) {
+        response = `I'm sorry, I'm having trouble processing your request.`;
+        if (end) conv.close(response);
+        else conv.ask(`${response}  Please try again.`);
+    } else {
+        if (end) {
+            conv.close(response);
+        } else {
+            if (typeof(response) === 'string') {
+                conv.ask(`${response}${noQuestion ? '' : `  ${endingQuestion}`}`);
+            } else {
+                if (response.context) {
+                    conv.contexts.set(response.context.name, 1, {value: response.context.value});
+                }
+                conv.ask(`${response.text}${noQuestion ? '' : `  ${endingQuestion}`}`);
+                if(response.gaResponse) conv.ask(response.gaResponse);
+            }
+        }
+    }
+};
+
+const app = dialogflow();
+// Intent handlers
+app.intent("Default Welcome Intent", conv => respond(conv, `Welcome to the Say What? Starter Action on Google! How can I help you?`, false, true));
+app.intent("Default Exit Intent", conv => respond(conv, `Farewell from the Say What? Starter Action on Google!`, true, false));
+app.intent("Default Fallback Intent", conv => respond(conv, `I have no idea what you just said.`));
+app.intent("Basic Fulfillment", conv => {
+    const response = getRandomItemFromArray([
+        `This is a basic response via webhook.  We can send back whatever we want from our endpoint for Dialogflow to say.`,
+        `In Dialogflow, we can just add multiple responses and Dialogflow will randomize them.  Here, we need to handle that ourselves via code (if we want to do that.)`
+    ]);
+    respond(conv, response);
+});
+app.intent("Contexts", conv => {
+    const params = conv.parameters || {};
+    respond(conv, {text: `Hey there ${params.firstName}!  How about some extra info about contexts?`}, false, true);
+    /*respond(conv, {
+        text: `Hey there ${params.firstName}!  How about some extra info about contexts?`,
+        context: {name: `firstName`, value: params.firstName}
+    }, false, true);*/
+});
+app.intent("Contexts - yes", conv => {
+    const params = conv.parameters || {};
+    respond(conv, `Wonderful, ${params.firstName}!   Contexts allow me to remember pieces of information from one statement to the next.  See how I remember your name is ${params.firstName} without you telling me again?`);
+});
+app.intent("Contexts - no", conv => {
+    const params = conv.parameters || {};
+    respond(conv, `Sorry to hear that, ${params.firstName}.  Maybe another time?`, false, true);
+});
+app.intent("Entities/Parameters", conv => {
+    const params = conv.parameters || {};
+    const contextParams = conv.contexts.get('_actions_on_google').parameters || {};
+
+    let responses = [];
+
+    if(params.city && params.room) {
+        responses.push(`You can even include multiple parameters in a single response.  In this case, a user mentioned both ${params.city} and ${params.room}.`);
+    } else if(params.city) {
+        responses.push(`Using the built-in entity "geo-city" will allow you to grab out an entered city, such as ${params.city}.`);
+    } else if(params.room) {
+        responses.push(
+            `You can create your own entities of any type to help your Action.  For example, someone can ask about the ${params.room} room at That Conference and we can react accordingly.`,
+            `If you have a parameter with synonyms, you can also reference "parameter.original" which refers to what the user actually said rather than just the reference value.  In most cases, they're the same, but sometimes they can differ.  There may be a difference between "${params.room}" and "${contextParams['room.original']}".`
+        );
+    } else {
+        responses.push(`Entities are context variables you can pull out of a user's phrase.  It could be built-in values like cities and countries or you can create your own.`);
+    }
+
+    respond(conv, getRandomItemFromArray(responses));
+});
+app.intent("Google Assistant", conv => {
+    respond(conv, {
+        text: `Cards consist of an image, a card title, a card subtitle, and interactive buttons (for sending user queries or opening links).`,
+        gaResponse: new BasicCard({
+            title: `Google Assistant Response`,
+            subtitle: `via a node.js webhook`,
+            image: {
+                url: `https://nodejs.org/static/images/logos/nodejs-new-pantone-black.png`,
+                accessibilityText: `node.js logo`
+            },
+            buttons: new Button({
+                title: `node.js Home`,
+                url: `https://nodejs.org/en/`
+            })
+        })
+    });
+});
+
+express().use(bodyParser.json(), app).listen(2323);
